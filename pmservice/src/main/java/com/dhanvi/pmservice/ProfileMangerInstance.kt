1e5d72c76b7bdf90de50f8815b32a77b284210aa
Dhanvi Desai <dhanvidesai99@gmail.com>
2023-01-07 17:09:27 +0530

    change connection logic from intent to component

--- pmservice/src/main/java/com/dhanvi/pmservice/ProfileMangerInstance.kt
+++ pmservice/src/main/java/com/dhanvi/pmservice/ProfileMangerInstance.kt
@@ -10,14 +10,22 @@ import android.util.Log
 class ProfileMangerInstance(private val context: Context) {
     val TAG = ProfileMangerInstance::class.java.simpleName
 
-    private val profileManagerIntent = Intent("com.dhanvi.profilemanagerservice.ProfileManagerService").setPackage("com.dhanvi.profilemanagerservice")
+    private val profileManagerIntent = Intent("com.dhanvi.profilemanagerservice.ProfileManagerService").setPackage("")
+
+    private val PROFILE_MANAGER_SERVICE_PACKAGE_NAME = "com.dhanvi.profilemanagerservice"
+    private val PROFILE_MANAGER_SERVICE_CLASS_NAME = "com.dhanvi.profilemanagerservice.ProfileManagerService"
 
     private var profileManagerService: IProfileManagerServiceAidl? = null
+
+    private var serviceConnectedCallback : ((IProfileManagerServiceAidl) -> Unit)? = null
 
     private val profileManagerConnection = object: ServiceConnection {
         override fun onServiceConnected(p0: ComponentName?, p1: IBinder?) {
             Log.d(TAG, "Connected to ProfileManager")
             profileManagerService = IProfileManagerServiceAidl.Stub.asInterface(p1)
+            serviceConnectedCallback?.let {
+                profileManagerService?.let(it)
+            }
         }
 
         override fun onServiceDisconnected(p0: ComponentName?) {
@@ -26,8 +34,11 @@ class ProfileMangerInstance(private val context: Context) {
 
     }
 
-    fun connect() {
-        context.bindService(profileManagerIntent, profileManagerConnection, Context.BIND_AUTO_CREATE)
+    fun connect(serviceConnectedCallback: (IProfileManagerServiceAidl) -> Unit): Boolean {
+        val intent = Intent()
+        intent.component = ComponentName(PROFILE_MANAGER_SERVICE_PACKAGE_NAME, PROFILE_MANAGER_SERVICE_CLASS_NAME)
+        this.serviceConnectedCallback = serviceConnectedCallback
+        return context.bindService(intent, profileManagerConnection, Context.BIND_AUTO_CREATE)
     }
 
     fun getServiceVersion(): String? {
